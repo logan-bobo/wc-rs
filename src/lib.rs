a817d82a935fb7dcf6101b3f5c00bb6801843259
Logan Cox <mail@logan-cox.com>
2024-10-10 08:56:03 +0100

    feat: count the chars in a file from the -m flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use core::str;
-use std::fs;
-use std::error::Error;
+use std::{fs, error::Error};
 
 use clap::Parser;
 
@@ -8,7 +7,7 @@ const NEWLINE: u8 = 10;
 
 #[derive(Parser, Debug)]
 pub struct Args {
-    #[arg(short = 'b', required = false)]
+    #[arg(short = 'c', required = false)]
     pub count_bytes: bool,
 
     #[arg(short = 'l', required = false)]
@@ -17,12 +16,15 @@ pub struct Args {
     #[arg(short = 'w', required = false)]
     pub count_words: bool,
 
+    #[arg(short = 'm', required = false)]
+    pub count_chars: bool,
+
     #[arg(required = true)]
     pub file_name: String,
 }
 
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
-    // What if the file is large?
+    // What if the file is large? (oom)
     let bytes = fs::read(&args.file_name)?;
 
     if args.count_bytes {
@@ -41,12 +43,16 @@ pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
         println!("\t{} {}", &args.file_name, words);
     }
 
+    if args.count_chars {
+        let chars = count_chars(&bytes);
+        println!("\t{} {}", &args.file_name, chars)
+    }
+
     Ok(())
 }
 
-
-fn count_lines(bytes: &Vec<u8>) -> u32 {
-    let mut new_line_count: u32 = 0;
+fn count_lines(bytes: &Vec<u8>) -> usize {
+    let mut new_line_count = 0;
 
     for byte in bytes {
         if *byte == NEWLINE {
@@ -62,6 +68,12 @@ fn count_words(bytes: &Vec<u8>) -> usize {
     let words_as_string: &str = str::from_utf8(bytes).expect("utf-8 error");
 
     words_as_string.split_ascii_whitespace().count()
+}
+
+fn count_chars(bytes: &Vec<u8>) -> usize {
+    let words_as_string: &str = str::from_utf8(&bytes).expect("utf-8 error");
+
+    words_as_string.chars().count()
 }
 
 #[cfg(test)]
@@ -80,8 +92,16 @@ mod test {
     #[test]
     fn count_words_in_file() {
         let content: Vec<u8> = "Hello\t World \n \n Today!   All!".as_bytes().to_vec();
-        let expected_words: usize = 4;
+        let expected_words = 4;
 
         assert_eq!(expected_words, count_words(&content));
+    }
+
+    #[test]
+    fn count_chars_in_file() {
+        let content = "Hello World".as_bytes().to_vec();
+        let expected_chars = 11;
+
+        assert_eq!(expected_chars, count_chars(&content))
     }
 }
