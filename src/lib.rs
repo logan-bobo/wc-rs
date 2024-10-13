016500326783068b01eeab5de40252eeacc872f0
Logan Cox <mail@logan-cox.com>
2024-10-13 09:27:31 +0100

    chore: address clippy errors and fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use core::str;
 use std::error::Error;
+use std::fs;
 use std::io::{self, Read};
 use std::str::Utf8Error;
-use std::{fs, usize};
 
 use clap::Parser;
 
@@ -10,19 +10,39 @@ const NEWLINE: u8 = 10;
 
 #[derive(Parser, Debug)]
 pub struct Args {
-    #[arg(short = 'c', required = false, help = "Count the bytes from a file or stdin")]
+    #[arg(
+        short = 'c',
+        required = false,
+        help = "Count the bytes from a file or stdin"
+    )]
     pub count_bytes: bool,
 
-    #[arg(short = 'l', required = false, help = "Count the lines from a file or stdin")]
+    #[arg(
+        short = 'l',
+        required = false,
+        help = "Count the lines from a file or stdin"
+    )]
     pub count_lines: bool,
 
-    #[arg(short = 'w', required = false, help = "Count the words from a file a stdin")]
+    #[arg(
+        short = 'w',
+        required = false,
+        help = "Count the words from a file a stdin"
+    )]
     pub count_words: bool,
 
-    #[arg(short = 'm', required = false, help = "Count the characters from a file or stdin")]
+    #[arg(
+        short = 'm',
+        required = false,
+        help = "Count the characters from a file or stdin"
+    )]
     pub count_chars: bool,
 
-    #[arg(help = "The path of the file you wish to use, for example ~/file.txt", required = false, default_value = "")]
+    #[arg(
+        help = "The path of the file you wish to use, for example ~/file.txt",
+        required = false,
+        default_value = ""
+    )]
     pub file_name: String,
 }
 
@@ -51,7 +71,7 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         println!("  {} {}", count_lines(&bytes), file_name);
     }
 
-    if args.count_words  {
+    if args.count_words {
         let words = count_words(&bytes)?;
 
         println!("  {} {}", words, file_name);
@@ -66,7 +86,8 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     if is_default_option(&args) {
         let words = count_words(&bytes)?;
 
-        println!("  {} {} {} {}",
+        println!(
+            "  {} {} {} {}",
             count_lines(&bytes),
             words,
             bytes.len(),
@@ -89,14 +110,14 @@ fn count_lines(bytes: &Vec<u8>) -> usize {
     new_line_count
 }
 
-fn count_words(bytes: &Vec<u8>) -> Result<usize, Utf8Error> {
+fn count_words(bytes: &[u8]) -> Result<usize, Utf8Error> {
     // we can handle this better
     let words_as_string: &str = str::from_utf8(bytes)?;
 
     Ok(words_as_string.split_ascii_whitespace().count())
 }
 
-fn count_chars(bytes: &Vec<u8>) -> Result<usize, Utf8Error> {
+fn count_chars(bytes: &[u8]) -> Result<usize, Utf8Error> {
     let words_as_string: &str = str::from_utf8(bytes)?;
 
     Ok(words_as_string.chars().count())
@@ -104,7 +125,7 @@ fn count_chars(bytes: &Vec<u8>) -> Result<usize, Utf8Error> {
 
 fn is_default_option(args: &Args) -> bool {
     if !args.count_bytes && !args.count_lines && !args.count_words && !args.count_chars {
-        return true
+        return true;
     }
 
     false
@@ -128,7 +149,7 @@ mod test {
         let content: Vec<u8> = "Hello\t World \n \n Today!   All!".as_bytes().to_vec();
         let expected_words = 4;
 
-        assert_eq!(expected_words, count_words(&content));
+        assert_eq!(expected_words, count_words(&content).unwrap());
     }
 
     #[test]
@@ -136,6 +157,6 @@ mod test {
         let content = "Hello World".as_bytes().to_vec();
         let expected_chars = 11;
 
-        assert_eq!(expected_chars, count_chars(&content))
+        assert_eq!(expected_chars, count_chars(&content).unwrap())
     }
 }
